539a798b0c30271ad18dd623abc4ce4666bc598e
unknown <abdulcader.ayaz@gmail.com>
2020-07-29 15:53:10 +0200

    Added login page

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,47 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
+import Login from "../views/Login.vue";
+import store from "../store";
 
 Vue.use(VueRouter);
+
+function isAuthenticated() {
+  const _token = localStorage.getItem("token");
+  console.log(_token);
+  return _token === null ? false : true;
+}
 
 const routes = [
   {
     path: "/",
+    redirect: {
+      name: "Home",
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      requiresAuth: false,
+    },
+  },
+  {
+    path: "/home",
     name: "Home",
     component: Home,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/about",
     name: "About",
     component: About,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
@@ -22,6 +50,17 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (isAuthenticated()) {
+      next();
+      return;
+    }
+    next("/login");
+  }
+  next();
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-row class="vh-100" align-v="center">
+    <b-col></b-col>
+    <b-col>
+      <b-card class="text-center">
+        <b-form @submit="onSubmit">
+          <b-form-group id="email" label="Adresse E-mail" label-for="email-input">
+            <b-form-input
+              id="email-input"
+              v-model="form.email"
+              type="email"
+              required
+              placeholder="Ex: pierre.dupont@outlook.com"
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group id="password" label="Mot de passe" label-for="password-input">
+            <b-form-input id="password-input" v-model="form.password" type="password" required></b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="primary" :disabled="loading">Connexion</b-button>
+        </b-form>
+        <b-spinner variant="primary" v-if="loading" class="spinner"></b-spinner>
+      </b-card>
+    </b-col>
+    <b-col></b-col>
+  </b-row>
+</template>
+
+<script>
+import { API_URL } from "../config";
+
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+      loading: false,
+    };
+  },
+  methods: {
+    async onSubmit(e) {
+      e.preventDefault();
+      this.loading = true;
+      await fetch(API_URL + "professional", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: this.form.email,
+          password: this.form.password,
+        }),
+      })
+        .then((response) => response.json())
+        .then(async (response) => {
+          this.loading = false;
+          if (response.error) {
+            alert(response.messages[0]);
+          } else {
+            await localStorage.setItem(
+              "token",
+              response.token_type + " " + response.access_token
+            );
+            this.$router.replace({ name: "Home" });
+          }
+        })
+        .catch((error) => {
+          this.loading = false;
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
