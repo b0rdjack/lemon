f7df1c287c49c57545a53ff019da5dc899c8bdc6
unknown <abdulcader.ayaz@gmail.com>
2020-08-01 17:17:19 +0200

    Added signup HTTP call

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -20,8 +20,10 @@
             Suivant
             <font-awesome-icon :icon="['fas', 'angle-double-right']" />
           </b-button>
+          <b-spinner variant="primary" v-if="loading" class="spinner"></b-spinner>
         </b-col>
       </b-row>
+
       <b-row class="text-center buttons" v-if="!first_step">
         <b-col>
           <router-link to="Login">Déjà un compte ? Connectez vous !</router-link>
@@ -35,6 +37,7 @@
 <script>
 import SignupForm from "@/components/signup/Form.vue";
 import PaymentForm from "@/components/signup/Payment.vue";
+import { API_URL } from "../config";
 export default {
   components: {
     SignupForm,
@@ -70,8 +73,38 @@ export default {
     },
     async onSubmit(e) {
       e.preventDefault();
-      console.log(this.form);
-      console.log(this.payment);
+      this.loading = true;
+      await fetch(API_URL + "professional/register", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          last_name: this.form.lastname,
+          first_name: this.form.firstname,
+          email: this.form.email,
+          password: this.form.password,
+          password_confirmation: this.form.confirm_password,
+          card: {
+            number: this.payment.card_number,
+            exp_month: this.payment.exp_month,
+            exp_year: this.payment.exp_year,
+            cvc: this.payment.cvc,
+          },
+        }),
+      })
+        .then((response) => response.json())
+        .then(async (response) => {
+          this.loading = false;
+          if (response.error) {
+            console.log(response);
+            alert("Les informations saisies ne sont pas valides.");
+          } else {
+            alert(response.messages[0]);
+            this.$router.replace({ name: "Login" });
+          }
+        });
     },
   },
 };
